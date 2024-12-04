c16342612226683c3ada3dedbcca667c82f218ac
Barbaraluiza202 <barbara.fernandes.coelho@escola.pr.gov.br>
2024-12-04 20:15:15 -0300

    Create movie.js

---
+++ movie.js
@@ -0,0 +1,137 @@
+export const movies = [
+{
+title: "
+genre: " comedia
+ageRating: 12 anos,
+synopsis: "Dona Hermínia é uma senhora de meia idade, divorciada do marido, que a trocou por uma mulher mais jovem. Hiperativa, ela não larga do pé de seus filhos Marcelina e Juliano. Um dia, após descobrir que eles a consideram chata, ela resolve sair de casa sem avisar ninguém, deixando todos preocupados. Dona Hermínia decide ir visitar a querida Tia Zélia para desabafar suas tristezas atuais e recordar os bons tempos do passado.
+https://www.youtube.com/watch?v=q0gZOGoRRnc
+
+},
+{
+title: "
+genre: "ação",
+ageRating: 14,
+synopsis: "Enquanto sua nova casa a noite, a pequena Coraline descobre uma porta secreta que contém um mundo parecido com o dela, porém melhor em muitas maneiras. Todos tinham botões no lugar dos olhos, os pais são carinhosos e os sonhos de Coraline viram realidade por lá. Ela se encanta com a descoberta, mas logo percebe segredos estranhos que estão em ação:uma outra  mãe e o resto da sua família tentam mantê-la eternamente neste mundo paralelo. 
+https://youtu.be/bsgxE1TjgiM?si=KSZEYMN6N3cav3nR
+},
+{
+title: "",
+genre: "",
+ageRating: 10,
+synopsis: "",Durante a segunda Guerra Mundial na Itália, o judeu Guido e seu filho Giosué sao levados para um campo de concentração nazista. Afastado, ele precisa usar sua imaginação para fazer o menino acreditar que estão participando de uma grande brincadeira, com o intuito de protegê-lo do terror e da violência ao redor.
+https://youtu.be/a4jKsZQJ7-g?si=i4WqbdSAlTZRcdKR
+},
+{
+
+
+
+
+title: "",
+genre: "",
+ageRating: 13
+synopsis: "",Um grupo de jovens enfrenta um assassino mascarado que testa seus conhecimentos sobre filmes de terror. A pequena cidade de Woodsboro nunca mais será a mesma.
+https://youtu.be/knovvPSPMSE?si=tYulittw1slK3ESX
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""Após um paciente cometer um suicídio brutal em sua frente, a psiquiatra Rose é perseguida por uma entidade maligna que muda de forma. Enquanto tenta escapar desse pesadelo, Rose também precisa enfrentar seu passado conturbado para sobreviver.
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 16,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 18,
+synopsis: "",
+trailer: ""
+},
+{
+title: "",
+genre: "",
+ageRating: 13,
+synopsis: "",
+trailer: ""
+}
+];
+
+
